6ca688f4ded289a7a274c49fa76810b317eee2a1
ssilangoss <ilango.sel87@gmail.com>
2019-03-16 17:59:11 +0530

    version changed

--- src/test/java/SampleGitTest.java
+++ src/test/java/SampleGitTest.java
@@ -1,5 +1,14 @@
 package test.java;
 
+import java.io.IOException;
+
+import org.testng.annotations.Test;
+
 public class SampleGitTest {
 
+	@Test
+	public void loginTest() throws IOException{
+		
+		WebDriver wd=new FirefoxDriver();
+	}
 }
